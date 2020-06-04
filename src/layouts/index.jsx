f610a231d0e03b62fd48a9db90dc534068d9ded8
Daniel Chapman <leptest@gmail.com>
2020-06-04 14:31:45 +0100

    npm update

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import SiteHeader from '../components/SiteHeader';
 import SiteFooter from '../components/SiteFooter';
 import './all.scss';
